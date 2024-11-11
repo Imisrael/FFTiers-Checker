d4a1b70f2df49061f83a19f7c0bb2ecffb6c40b1
Israel Imru <israelimru@Israels-Mac.local>
2024-11-11 12:37:42 -0800

    changed around struct to use pointer methods instead of just functions

--- web/main.go
+++ web/main.go
@@ -1,151 +1,145 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
-    "io"
-    "fmt"
-    "os"
-    "net/http"
-    "regexp"
-    "strings"
-    "time"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+	"time"
 )
 
 const (
-    Qb = "https://s3-us-west-1.amazonaws.com/fftiers/out/text_QB.txt"
-    Dst = "https://s3-us-west-1.amazonaws.com/fftiers/out/text_DST.txt"
-    Rb = "https://s3-us-west-1.amazonaws.com/fftiers/out/text_RB"
-    Wr = "https://s3-us-west-1.amazonaws.com/fftiers/out/text_WR"
-    Te =  "https://s3-us-west-1.amazonaws.com/fftiers/out/text_TE"
-    Flex = "https://s3-us-west-1.amazonaws.com/fftiers/out/text_FLX"
+	Qb   = "https://s3-us-west-1.amazonaws.com/fftiers/out/text_QB.txt"
+	Dst  = "https://s3-us-west-1.amazonaws.com/fftiers/out/text_DST.txt"
+	Rb   = "https://s3-us-west-1.amazonaws.com/fftiers/out/text_RB"
+	Wr   = "https://s3-us-west-1.amazonaws.com/fftiers/out/text_WR"
+	Te   = "https://s3-us-west-1.amazonaws.com/fftiers/out/text_TE"
+	Flex = "https://s3-us-west-1.amazonaws.com/fftiers/out/text_FLX"
 )
 
 const (
-    Standard int = iota
-    Half
-    Full
+	Standard int = iota
+	Half
+	Full
 )
 
 type tier struct {
-    rulesFormat int
-    playerPosition string
-    players map[int][]string
-    date time.Time
+	rulesFormat int
+	url         string
+	players     map[int][]string
+	date        time.Time
 }
 
-func urlBuilder(name tier) string {
-    s := ""
-    if !(name.playerPosition == Qb || name.playerPosition == Dst ) {
-        switch name.rulesFormat {
-            case 1:
-                s = "-HALF.txt"
-            case 2:
-             s = "-PPR.txt"
-            default:
-                s = ".txt"
+func (t *tier) GetUrl() {
 
-        }
-    }
-    fullUrl := name.playerPosition + s
-    fmt.Println(fullUrl)
-    return fullUrl
+	if !(t.url == Qb || t.url == Dst) {
+		switch t.rulesFormat {
+		case 1:
+			t.url = t.url + "-HALF.txt"
+		case 2:
+			t.url = t.url + "-PPR.txt"
+		default:
+			t.url = t.url + ".txt"
+		}
+	}
 }
 
-func constGetter (playerPosition string) string {
-    var retVal string
-    switch playerPosition {
-        case "FLEX":
-            retVal = Flex
-        case "QB": 
-            retVal = Qb
-        case "WR":
-            retVal = Wr
-        case "RB":
-            retVal = Rb
-        case "TE":
-            retVal = Te
-        case "DST":
-            retVal = Dst
-        default:
-            retVal = Flex
-    }
-    return retVal
+func (t *tier) getUrlBasedOnPos(pos string) {
+	switch pos {
+	case "FLEX":
+		t.url = Flex
+	case "QB":
+		t.url = Qb
+	case "WR":
+		t.url = Wr
+	case "RB":
+		t.url = Rb
+	case "TE":
+		t.url = Te
+	case "DST":
+		t.url = Dst
+	default:
+		t.url = Flex
+	}
 }
 
-func formatGetter (rulesFormat string) int {
-    var retVal int
+func (t *tier) formatGetter(rulesFormat string) {
 
-    switch rulesFormat {
-        case "STANDARD":
-            retVal = 0
-        case "HALF":
-            retVal = 1
-        case "PPR":
-            retVal = 2
-        default: 
-            retVal = 0
-    }
+	switch rulesFormat {
+	case "STANDARD":
+		t.rulesFormat = 0
+	case "HALF":
+		t.rulesFormat = 1
+	case "PPR":
+		t.rulesFormat = 2
+	default:
+		t.rulesFormat = 0
+	}
 
-    return retVal
+}
+
+func initObj() tier {
+	obj := tier{0, "", make(map[int][]string), time.Now()}
+	playerPosition := strings.ToUpper(os.Args[1])
+	rulesFormat := strings.ToUpper(os.Args[2])
+	obj.getUrlBasedOnPos(playerPosition)
+	obj.formatGetter(rulesFormat)
+	obj.GetUrl()
+	return obj
 }
 
 func main() {
 
-    playerPosition := strings.ToUpper(os.Args[1])
-    playerPosition = constGetter(playerPosition)
+	obj := initObj()
+	resp, err := http.Get(obj.url)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    rulesFormat := strings.ToUpper(os.Args[2])
-    formatInt := formatGetter(rulesFormat)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    half := tier{formatInt, playerPosition, make(map[int][]string), time.Now()}
-    url := urlBuilder(half)
+	location, err := time.LoadLocation("America/Los_Angeles")
+	if err != nil {
+		panic(err)
+	}
+	lastUpdated := resp.Header["Last-Modified"][0]
+	date, err := time.Parse(time.RFC1123, lastUpdated)
+	if err != nil {
+		panic(err)
+	}
+	localTime := date.In(location)
+	obj.date = localTime
 
-    resp, err := http.Get(url)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	re := regexp.MustCompile(`[0-9]+\:\s`)
+	player := os.Args[3]
+	findPlayerRe := regexp.MustCompile(`(?i)([A-Za-z]+\s)?` + player + `(\s[A-Za-z]+)?`)
+	mapOfTiers := make((map[int]string))
+	sb := string(body)
+	newString := strings.Split(sb, "Tier")
+	for idx, val := range newString {
+		tier := re.ReplaceAllString(val, "")
+		mapOfTiers[idx] = tier
+	}
 
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	var tier int
+	for ti, val := range mapOfTiers {
+		testing := strings.Split(val, ",")
+		tier = ti
+		for _, t := range testing {
+			st := findPlayerRe.FindString(t)
+			if st != "" {
+				fmt.Println(tier, st)
+				obj.players[tier] = append(obj.players[tier], st)
+			}
+		}
+	}
 
-    location, err := time.LoadLocation("America/Los_Angeles")
-    if err != nil {
-        panic(err)
-    }
-    lastUpdated := resp.Header["Last-Modified"][0]
-    date, err := time.Parse(time.RFC1123, lastUpdated)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println(date)
-    localTime := date.In(location)
-    half.date = localTime
-
-    re := regexp.MustCompile(`[0-9]+\:\s`)
-    player := os.Args[3]
-    findPlayerRe := regexp.MustCompile(`(?i)([A-Za-z]+\s)?` + player  + `(\s[A-Za-z]+)?`)
-    mapOfTiers := make((map[int]string))
-    sb := string(body)
-    newString := strings.Split(sb, "Tier")
-    for idx, val := range newString {
-        tier := re.ReplaceAllString(val, "")
-        mapOfTiers[idx] = tier
-    }
-
-    var tier int
-    for ti, val := range mapOfTiers {
-        testing := strings.Split(val, ",")
-        tier = ti
-        for _, t := range testing {
-            st := findPlayerRe.FindString(t)
-            if st != "" {
-                fmt.Println(tier, st)
-                half.players[tier] = append(half.players[tier], st) 
-            }
-        }
-    }
-
-    fmt.Println(half)
+	fmt.Println(obj)
+	fmt.Println(obj.date)
 }
